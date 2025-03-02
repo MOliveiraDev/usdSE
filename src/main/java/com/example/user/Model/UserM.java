bccb1fd09bf7c23ab602477bc20ba1fc6260f494
Oliver.dev <oliveiramaicon16637@gmail.com>
2025-03-02 12:55:28 -0300

    a

---
+++ src/main/java/com/example/user/Model/UserM.java
@@ -0,0 +1,46 @@
+package com.example.user.Model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+public class UserM {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String password;
+
+    public UserM() {
+    }
+
+    public UserM(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
